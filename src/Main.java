cdb789de7f36f18b9d61a189138fc5dab8cc9411
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2019-06-12 22:44:02 -0500

    fixed it

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,80 @@
 import com.maschel.roomba.RoombaJSSC;
 import com.maschel.roomba.RoombaJSSCSerial;
+import com.maschel.roomba.song.RoombaNote;
+import com.maschel.roomba.song.RoombaNoteDuration;
+import com.maschel.roomba.song.RoombaSongNote;
+import edu.wpi.first.networktables.NetworkTable;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String...args){
+    public static void main(String... args) {
         RoombaJSSC roomba = new RoombaJSSCSerial();
 
+
+
 // Connect
-        while(!roomba.connect("/dev/a/serial/port")){ // Use portList() to get available ports.
+        while (!roomba.connect("/dev/ttyUSB0")) { // Use portList() to get available ports.
             String[] ports = roomba.portList(); // Get available serial port(s) (not mandatory)
             System.out.println("Number of ports: " + ports.length);
-            for(String s:ports) System.out.println(s);
+            for (String s : ports) System.out.println(s);
         }
-
 
 // Make roomba ready for communication & control (safe mode)
         roomba.startup();
+        roomba.fullMode();
+        roomba.sleep(500);
 
-// Send commands
-        roomba.clean(); // Roomba will start cleaning
-        roomba.digitLedsAscii('H', 'E', 'Y', '!'); // Shows message on digit leds
+        roomba.digitLedsAscii('G','N','U','C');
+
+        RoombaSongNote[] notes = {
+                new RoombaSongNote(RoombaNote.E2, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.D2Sharp, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.E2, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.D2Sharp, RoombaNoteDuration.EightNote),
+
+                new RoombaSongNote(RoombaNote.E2, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.B1, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.D2, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.C2, RoombaNoteDuration.EightNote),
+
+                new RoombaSongNote(RoombaNote.A1, RoombaNoteDuration.QuarterNote),
+                new RoombaSongNote(RoombaNote.Pause, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.C1, RoombaNoteDuration.EightNote),
+                new RoombaSongNote(RoombaNote.E1, RoombaNoteDuration.EightNote),
+                        new RoombaSongNote(RoombaNote.A1, RoombaNoteDuration.EightNote),
+                        new RoombaSongNote(RoombaNote.B1, RoombaNoteDuration.QuarterNote),
+                        new RoombaSongNote(RoombaNote.Pause, RoombaNoteDuration.EightNote),
+                        new RoombaSongNote(RoombaNote.E1, RoombaNoteDuration.EightNote)
+        };
+// Save to song number 0, tempo (in BPM) 125
+        roomba.song(0, notes, 125);
+// Play song 0
+        roomba.play(0);
 
 // Read sensors (until key is pressed)
-        while (true) {
-            try {
-                if (!(System.in.available() == 0)) break;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        while (!roomba.buttonClockPressed()) {
+            roomba.sleep(100);
+            roomba.updateSensors();
 
-            System.out.println("Left wheel drop: " + roomba.wheelDropLeft() + " Right wheel drop: " + roomba.wheelDropRight());
-            roomba.updateSensors(); // Read sensor values from roomba
-            roomba.sleep(50); // Sleep for AT LEAST 50 ms
+            /*
+            if(roomba.bumpLeft()) System.out.println("Bumped left");
+            if(roomba.bumpRight()) System.out.println("Bumped right");
+            if(roomba.wall()) System.out.println("Hit wall");
+            if(roomba.cliffLeft()) System.out.println("Cliff left");
+            if(roomba.cliffRight()) System.out.println("Cliff right");
+            if(roomba.cliffFrontLeft()) System.out.println("Cliff front left");
+            if(roomba.cliffFrontRight()) System.out.println("Cliff front right");
+            System.out.println("Encoder left: " + roomba.encoderCountsLeft() + " Encoder right: " + roomba.encoderCountsRight());
+            */
 
-            // Read sensors
-            if (roomba.wall())
-                System.out.println("Wall detected!");
+
+            if (roomba.buttonCleanPressed())roomba.drivePWM(80, 80);
+             else if(roomba.buttonDayPressed()) roomba.drivePWM(60,-60);
+             else if(roomba.buttonMinutePressed()) roomba.drivePWM(-60, 60);
+             else if(roomba.buttonSchedulePressed()) roomba.play(0);
+             else roomba.drivePWM(0, 0);
+
         }
 
 // Return to normal (human control) mode
@@ -44,5 +82,6 @@ public class Main {
 
 // Close serial connection
         roomba.disconnect();
+
     }
 }
